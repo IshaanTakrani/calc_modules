a9e0fdf95954be3486a628005691ce443a43816b
IshaanTakrani <ishaantakrani@gmail.com>
2024-05-19 22:51:22 -0400

    added support for calculations using degrees

--- trig.py
+++ trig.py
@@ -24,17 +24,19 @@ def factorial(x):
     return fac
 
 
-def taylor_sin(x,n=20):
+def taylor_sin(x,n=20, mode="rad"):
 
     '''
     Approximates sin(x), accurate to 8 decimal places at x = 5000 using a Taylor series expansion
 
     Args:
-        x (int/float): x at which sin(x) is to be calculated
+        x (float): x at which sin(x) is to be calculated
         n (int): INITIALIZED to 20, number of terms to be added to sum
+        mode (string): INITIALIZED to rad, for radians, can be set to "deg" for degrees
 
     Returns:
         sinx (float): approximation of sin(x)
+        
 
     Time complexity:
         Worst case: O(n^2)
@@ -42,18 +44,20 @@ def taylor_sin(x,n=20):
 
     '''
 
+    if(mode == "deg"):
+        x = x/(180/3.141592653589793)
+
     def factorial(x):
         fac = 1
         for i in range(1,x+1):
             fac = fac * i
         return fac
 
-    twopi = 6.283185307179586
-
+ 
     # subtract (approximate) period from x, to avoid overflow
-    if(x > twopi):
-        while(x > twopi):
-            x -= twopi
+    if(x > 6.283185307179586):
+        while(x > 6.283185307179586):
+            x -= 6.283185307179586
 
     sinx = 0
     for i in range(n):
@@ -62,5 +66,9 @@ def taylor_sin(x,n=20):
         sinx += (num/den) * x**((2*i) + 1)
 
     return sinx
+    
 
 
+    
+
+print(taylor_sin(0.78, mode="deg"))
