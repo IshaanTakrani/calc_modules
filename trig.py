1108a7effd9c9c5f45c30e2d56cb67db71030798
IshaanTakrani <ishaantakrani@gmail.com>
2024-05-29 15:01:35 -0400

    added taylor_cos

--- trig.py
+++ trig.py
@@ -23,6 +23,8 @@ def factorial(x):
     return fac
 
 
+# Approximations for sin(x)
+
 def taylor_sin(x, mode,n=20):
     '''
     Approximates sin(x) using a Taylor series expansion
@@ -98,3 +100,48 @@ def bhaskara_sin(x, mode):
         sinx = -(4 * x * (180 - x)) / (40500 - x * (180 - x))
     
     return sinx
+
+
+# Approximations of cos(x)
+
+def taylor_cos(x, mode,n=20):
+    '''
+    Approximates cos(x) using a Taylor series expansion
+    IDEAL range: 0-2pi radians (0-360 degrees)
+
+    Args:
+        x (float): x at which cos(x) is to be calculated
+        mode (string): mode, "deg" for degrees or "rad" for radians
+        n (int): INITIALIZED to 20, number of terms to be added to sum
+
+    Returns:
+        cosx (float): approximation of cos(x)
+        
+
+    Time complexity:
+        Worst case: O(n^2)
+        Best case: O(1)
+
+    '''
+
+    if (mode not in ['deg','rad']):
+        return ValueError("Error: Mode must be either 'deg' for degrees or 'rad' for radians for taylor_cos")
+
+    if(mode == "deg"):
+        x = x/(180/3.141592653589793238)
+
+    def factorial(x):
+        fac = 1
+        for i in range(1,x+1):
+            fac = fac * i
+        return fac
+
+    x = x % 6.283185307179586
+
+    cosx = 0
+    for i in range(n):
+        num = ((-1)**i)
+        den = factorial((2*i))
+        cosx += (num/den) * (x**(2*i))
+
+    return cosx
